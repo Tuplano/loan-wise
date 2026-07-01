c92e8ec858228e19e043a27b7bdc062b2e09eda2
Kevin Tuplano <kevintuplano11@gmail.com>
2026-07-01 14:11:33 +0800

    feat: add migration and seed when opening app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import { db } from '@/db/client';
+import migrations from '@/db/migrations/migrations';
+import { seedDefaultCategories } from '@/db/seed';
 
 import '@/global.css';
 
@@ -11,10 +16,22 @@ SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { success, error } = useMigrations(db, migrations);
+
+  useEffect(() => {
+    if (success) {
+      seedDefaultCategories();
+    }
+  }, [success]);
+
+  if (error) {
+    throw error;
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
-      <AppTabs />
+      {success && <AppTabs />}
     </ThemeProvider>
   );
 }
